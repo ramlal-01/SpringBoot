e4059861bd59603397d7bb7e54e82714b55d456d
ramlal-01 <ramlal0801@gmail.com>
2026-01-19 15:27:53 +0530

    API Structure

--- Day2_React/src/main/java/com/example/day2_react/controller/Home.java
+++ Day2_React/src/main/java/com/example/day2_react/controller/Home.java
@@ -4,6 +4,8 @@ import com.example.day2_react.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class Home {
 
@@ -13,5 +15,17 @@ public class Home {
         StudentModel student = new StudentModel(1, "Ram" , "ram@gmail.com");
 
         return student;
+    }
+
+    @GetMapping("/userdata")
+    public ArrayList<StudentModel> getStudents() {
+        ArrayList<StudentModel> students = new ArrayList<>();
+        students.add(new StudentModel(1, "John", "Doe"));
+        students.add(new StudentModel(2, "Aditya", "adi@gmail.com"));
+        students.add(new StudentModel(3, "Ram", "ram@gmail.com"));
+        students.add(new StudentModel(4, "Remanshu", "remanshu@gmail.com"));
+        students.add(new StudentModel(5, "Aman", "Aman@gmail.com"));
+
+        return students;
     }
 }
