ecc207fb785ecbd926cf69b7e28062038b59c206
ramlal-01 <ramlal0801@gmail.com>
2026-01-19 14:52:45 +0530

    day2 react

---
+++ Day2_React/src/test/java/com/example/day2_react/Day2ReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day2_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day2ReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
